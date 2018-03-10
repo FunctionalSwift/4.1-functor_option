9d9530b6a50da83709cb92dfad80d65bb16d11da
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-10 23:37:43 +0100

    Bye bye execution time errors. Hello compilation time errors?

--- Functor Option.playground/Contents.swift
+++ Functor Option.playground/Contents.swift
@@ -4,3 +4,18 @@ enum Option<A> {
     case none
     case some(A)
 }
+
+class TextField {
+    let text: Option<String> = .none
+}
+
+func send(_ username: String) {
+    //Logic to send username
+}
+
+func sendUserNameOrAlert(textField: TextField) {
+    switch textField.text {
+    case .none: print("ERROR: TextField empty")
+    case let .some(username): send(username)
+    }
+}
